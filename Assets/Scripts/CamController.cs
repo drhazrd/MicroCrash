aa4941bc477e3f8c141fc78a5db882ef7135289b
Nick Hatcher Alpha <nick@colossalrobot.com>
2020-03-14 16:34:21 -0400

    Cam Controller

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public Transform target;
+    public Camera mainCamera;
+    public Transform camPiviot;
+    public Vector3 playerPosition;
+    public Vector3 cameraOffset;
+    public float viewAngleX, viewAngleY, viewAngleZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camPiviot.transform.position = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //playerPosition = new Vector3 (playerPosition.x + cameraOffset.x, 7f, playerPosition.z + cameraOffset.z);
+        transform.LookAt(target);
+        transform.position = target.position - cameraOffset;
+        mainCamera.transform.rotation = Quaternion.Euler(viewAngleX, viewAngleY, viewAngleZ);
+    }
+}
---
+++ Assets/Scripts/GroupObjects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GroupObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
