e41e0774f51defc329a8c90f5c8f8a4b875d1d3c
Nick Hatcher Alpha <nick@colossalrobot.com>
2020-03-16 23:12:12 -0400

    KartTestMovement

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -5,23 +5,19 @@ using UnityEngine;
 public class CamController : MonoBehaviour
 {
     public Transform target;
-    public Camera mainCamera;
-    public Transform camPiviot;
-    public Vector3 playerPosition;
+
     public Vector3 cameraOffset;
-    public float viewAngleX, viewAngleY, viewAngleZ;
+    public Transform camPiviot;
     // Start is called before the first frame update
     void Start()
     {
-        camPiviot.transform.position = target.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        //playerPosition = new Vector3 (playerPosition.x + cameraOffset.x, 7f, playerPosition.z + cameraOffset.z);
-        transform.LookAt(target);
         transform.position = target.position - cameraOffset;
-        mainCamera.transform.rotation = Quaternion.Euler(viewAngleX, viewAngleY, viewAngleZ);
+        transform.LookAt(target);
+        camPiviot.transform.position = target.transform.position;
     }
 }
