02680945365b0fc77d4b0a3b93c658e338985eaa
Nick Hatcher Pro <nick@colossalrobot.com>
2020-04-04 06:17:12 -0400

    Meta Evenflow

--- Assets/Scripts/Archive/zRCController.cs
+++ Assets/Scripts/Archive/zRCController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RCController : MonoBehaviour
+public class zRCController : MonoBehaviour
 {
     public Rigidbody sphere;
     public Transform kartModel;
